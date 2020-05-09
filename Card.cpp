e8a504ffec0732b50b0ec4dfed5761082b891688
Adam Aleb <adam.aleb@etu.univ-st-etienne.fr>
2020-05-09 19:53:42 +0200

    Fusion branchAdam

--- Card.cpp
+++ Card.cpp
@@ -4,25 +4,6 @@ Card::Card(Shape shape, int _nb) : shape(shape),rank(_nb){
 	
 }
 
-void Card::display() {
 
-	switch (rank) {
-	case 1: 
-		cout << "As de " << map(shape) << endl;
-		break;
-	case 13:
-		cout << "Roi" << " de " << map(shape) << endl;
-		break;
-	case 12:
-		cout << "Reine" << " de " << map(shape) << endl;
-		break;
-	case 11:
-		cout << "Valet" << " de " << map(shape) << endl;
-		break;
-	default:
-		cout << rank << " de " << map(shape) << endl;
-		break;
-	}
-	
 
 }
